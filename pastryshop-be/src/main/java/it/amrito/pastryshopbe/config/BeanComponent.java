75573a2b04386067810ddda58c45b62a664c1aa9
amritoV <verrelliamrito94@gmail.com>
2023-02-20 11:02:37 +0100

    clean up

--- pastryshop-be/src/main/java/it/amrito/pastryshopbe/config/BeanComponent.java
+++ pastryshop-be/src/main/java/it/amrito/pastryshopbe/config/BeanComponent.java
@@ -4,7 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
